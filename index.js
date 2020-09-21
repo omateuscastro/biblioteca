8a365f3ad412d20b878444e8090fc92ff4a02176
unknown <mcastro.mateus@gmail.com>
2020-09-20 22:02:10 -0300

    Crud de Livros.

--- index.js
+++ index.js
@@ -14,13 +14,58 @@ app.get("/", function(req, res) {
 });
 
 app.get("/livros", function(req, res) {
-  res.render("livros/livros");
+  Livros
+      .findAll({ order: ["titulo"] })
+      .then(function (livros) {
+          res.render("livros/livros", { livros });
+      });
 });
 
 app.get("/livros/novo", function(req, res) {
-  let mensagens = [];
+  res.render("livros/novo", {  mensagem: "" });
+});
 
-  res.render("livros/novo", { mensagens });
+app.post("/livros/salvar", function (req, res) {
+  let titulo = req.body.titulo;
+  let autor = req.body.autor;
+  let editora = req.body.editora;
+  let ano = req.body.ano;
+  Livros
+      .create({ titulo, autor, editora, ano })
+      .then(
+          res.render("livros/novo", { mensagem: "Livro incluído."})
+      );
+});
+
+app.get("/livros/editar/:id", function (req, res) {
+  let id = req.params.id;
+  Livros
+      .findByPk(id)
+      .then(function (livro) {
+        res.render("livros/editar", { livro });
+      });
+});
+
+app.post("/livros/atualizar", function (req, res) {
+  let id = req.body.id;
+  let titulo = req.body.titulo;
+  let autor = req.body.autor;
+  let editora = req.body.editora;
+  let ano = req.body.ano;
+  Livros
+      .update({ titulo, autor, editora, ano }, { where: { id: id } })
+      .then(function () {
+          res.redirect("/livros");
+      });
+});
+
+app.get("/livros/excluir/:id", function (req, res) {
+  let id = req.params.id;
+  Livros
+      .destroy({ where: { id: id } })
+      .then(function () {
+          res.redirect("/livros");
+      })
 });
 
 app.listen(3000);
